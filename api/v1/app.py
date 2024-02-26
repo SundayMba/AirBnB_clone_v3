6d13d1634f465c795add6a3786d5a361d1287d80
Sundaymba <sundinoh@gmail.com>
2024-02-26 14:04:49 +0000

    added error file

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 starts a flask web application
 """
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -25,6 +25,26 @@ def removed_db(exception):
     """
     storage.close()
 
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 errors, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': 'Not found'}
+    return make_response(jsonify(message), code)
+
+
+@app.errorhandler(400)
+def handle_404(exception):
+    """
+    handles 400 errros, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
 
 if __name__ == "__main__":
     app.run(host=host, port=port, threaded=True)
